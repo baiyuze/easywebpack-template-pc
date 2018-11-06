098c6b60f27d5ff4d7af6974ae027aae87ad34bb
baiyuze <1347417308@qq.com>
2018-11-06 17:20:40 +0800

    修复:默认使用webkit内核

--- app/web/common/layout.jsx
+++ app/web/common/layout.jsx
@@ -13,6 +13,9 @@ export default class Layout extends Component {
           <meta name="viewport" content="width=device-width, initial-scale=1.0,minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
           <meta name="keywords" content={this.props.keywords}></meta>
           <meta name="description" content={this.props.description}></meta>
+          <meta http-equiv="expires" content='0' /> 
+          <meta name="renderer" content="webkit" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"></link>
         </head>
         <body>
